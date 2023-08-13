a9cd9f285a96fcd021e7eabe6b53f71861a066cd
Gokul <gokulgokul9597@gmail.com>
2023-08-13 20:30:27 +0530

    added footer

--- app.js
+++ app.js
@@ -5,4 +5,12 @@ function changeColor(color){
     const circle = document.querySelector
     ('.circle');
     circle.style.background = color;
+    const learn = document.querySelector
+    ('.learn');
+    learn.style.background = color;
+    const icons = document.querySelector
+    ('.icons');
+    icons.style.background = color;
+    // const circle = document.querySelector
+    // ('.circle');
 }
